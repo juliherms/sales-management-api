df2ee97e39772fe569fc9299d30dce680e03b1e0
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-08-07 20:05:10 -0300

    first commit

---
+++ src/main/java/com/github/juliherms/sm/SalesManagementApplication.java
@@ -0,0 +1,16 @@
+package com.github.juliherms.sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Sales Management API - Principal Class
+ */
+@SpringBootApplication
+public class SalesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/juliherms/sm/SalesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.juliherms.sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
