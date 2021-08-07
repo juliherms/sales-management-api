579ae83ee0d57ac207ede3bd1c32887dca394ba2
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-08-07 20:29:54 -0300

    create category repository

---
+++ src/main/java/com/github/juliherms/sm/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.github.juliherms.sm.repositories;
+
+import com.github.juliherms.sm.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * This class responsible to provide access to Category Model
+ */
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
